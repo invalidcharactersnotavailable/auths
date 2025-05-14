8f86809139a9b8180568d7aee0a45d05ad95a548
invalidcharactersnotavailable <invalidcharactersnotavailable@gmail.com>
2025-05-14 22:01:26 +0000

    modified:   .gitignore 	modified:   bun.lock 	modified:   package.json 	modified:   router/main.ts 	new file:   router/user/main.ts 	new file:   router/user/settings.ts 	new file:   views/auth/login.ejs 	new file:   views/auth/register.ejs 	new file:   views/user/settings.ejs

--- router/main.ts
+++ router/main.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import apiRouter from "./api/main";
+import user from "./user/main";
 const router = express.Router();
 
 router.use("/api", apiRouter);
+router.use("/user", user);
 
 export default router;
